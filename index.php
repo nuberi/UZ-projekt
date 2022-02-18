bf7da2ebec4babf93be84fd9592f3b43785ad587
nuberi <nuber.imre@gmail.com>
2022-02-18 23:14:41 +0100

    Added deloyment mode

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+if($_SERVER['DEPLOYMENT_MODE']==="DEV"){
+
+    ini_set('display_errors','1');
+    ini_set('display_startup_errors','1');
+    error_reporting(E_ALL);
+
+
+}
 
 require './router.php';
 require './slugifier.php';
