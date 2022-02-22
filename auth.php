9598d2e6488935dac7b0a75ed44edb856092b893
nuberi <nuber.imre@gmail.com>
2022-02-22 21:18:05 +0100

    Login-registration page update

--- auth.php
+++ auth.php
@@ -1,65 +1,71 @@
 <?php
-function logoutHandler(){
+function logoutHandler()
+{
     session_start();
-    $params=session_get_cookie_params();
-    setcookie(session_name(),'',0,$params['path'],$params['domain'],$params['secure'],isset($params['httponly']));
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', 0, $params['path'], $params['domain'], $params['secure'], isset($params['httponly']));
     session_destroy();
     header('Location: /');
 }
-function loginHandler(){
-    $pdo= getConnection();
-    $stmt= $pdo->prepare('SELECT * FROM users WHERE email = ?');
+function loginHandler()
+{
+    $pdo = getConnection();
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE email = ?');
     $stmt->execute([$_POST['email']]);
     $user = $stmt->fetch();
-    if(!$user){
+    if (!$user) {
         header('Location: /admin?info=invalidCredentials');
         return;
     }
-   
-    if (!password_verify($_POST['password'],$user['password'])){
+
+    if (!password_verify($_POST['password'], $user['password'])) {
         header('Location: /admin?info=invalidCredentials');
         return;
     }
+
+    session_start();
+    $_SESSION['admin'] = $user['admin'];
+    $_SESSION['userId'] = $user['id'];
+
     if ($user['admin']) {
-        session_start();
-        $_SESSION['admin']=$user['admin'];
-        $_SESSION['userId']=$user['id'];
+
         header('Location:/admin');
-    }
-    else {
-        session_start();
-        $_SESSION['admin']=$user['admin'];
-        $_SESSION['userId']=$user['id'];
+    } else {
+
         header('Location:/');
     }
 }
 //A felhaszáló be van-e jelentkezve?
-function isLoggedIn(){
-    if(!isset($_COOKIE[session_name()])){
+function isLoggedIn()
+{
+    if (!isset($_COOKIE[session_name()])) {
         return false;
     }
-    if(!isset($_SESSION)){
+    if (!isset($_SESSION)) {
         session_start();
     }
-    if(!isset($_SESSION['userId'])){
+    if (!isset($_SESSION['userId'])) {
         return false;
     }
-    
+
     return true;
 }
-function isAdmin(){
+function isAdmin()
+{
     if (!isset($_SESSION['admin'])) {
         return false;
     }
-    if ($_SESSION['admin']==="false") {
+    if ($_SESSION['admin'] === "false") {
         return false;
     }
-    if ($_SESSION['admin']==="true") {
+    if ($_SESSION['admin'] === "true") {
         return true;
     }
+    return false;
 }
-function redirectToLoginPageNotLoggedIn(){
-    if(isLoggedIn()){
+function redirectToLoginPageNotLoggedIn()
+{
+    if (isLoggedIn()) {
         return;
     }
     header('Location: /admin');
